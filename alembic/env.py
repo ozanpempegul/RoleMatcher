39b547d90decb776cc7ebe5a9e3a3c491eedfc3f
ozanp <ozanpembegul@gmail.com>
2025-10-13 21:09:01 +0300

    first migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,11 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 import os
 from dotenv import load_dotenv
+from models import Base
+
 
 # Load .env variables
 load_dotenv()
@@ -35,7 +35,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
