0f4d9ec9eaba585973e94670f2c46129a22a91a5
ozanp <ozanpembegul@gmail.com>
2026-06-14 14:17:43 +0300

    Added app name to window title

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from views.main_window.vm_main_window import*
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setApplicationName("RoleMatcher")
+    app.setApplicationDisplayName("RoleMatcher")  # optional; user-facing name
+    app.setOrganizationName("RoleMatcher")        # optional; used by QSettings paths
     main_window = MainWindow()
+    main_window.setWindowTitle(app.applicationDisplayName())
     main_window.show()
     app.exec()
